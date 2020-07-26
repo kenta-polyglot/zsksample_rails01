d6398ad3303433a19d5589573449330ee1a8f77b
endo-kotaro <umaibou1126@gmail.com>
2020-07-26 21:40:55 +0900

    Revert "complete user-test"
    This reverts commit 71d573cffffef8828ef0ed04c551b41f147a0f0c.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-
-  # name(test), email(test@example.com)
-  it "is valid with name and email" do
-    user = FactoryBot.create(:user)
-    user = User.new(name: 'sample', email: 'sample@example.com')
-    expect(user).to be_valid
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
 end
