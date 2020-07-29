6cbe8879a858b10b0e997e093aa73bdd1c6f33be
endo-kotaro <umaibou1126@gmail.com>
2020-07-29 15:23:03 +0900

    user_models test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "正常ケースのテスト" do
+    user = User.new(name: 'sample', email: 'sample@example.com')
+    expect(user).to be_valid
+  end
+
+  it "異常ケース：nameが空文字" do
+    user = User.new(name: nil, email: 'sample@example.com')
+    expect(user).to be_invalid
+  end
+
+  it "異常ケース：emailが空文字" do
+    user = User.new(name: 'sample', email: nil)
+    expect(user).to be_invalid
+  end
+
+  it "異常ケース：nameが2文字未満" do
+    user = User.new(name: 'a', email: 'sample@example.com')
+    expect(user).to be_invalid
+    expect(user.errors[:name]).to include("is too short (minimum is 2 characters)")
+  end
+
+  it "異常ケース：nameが13文字以上" do
+    user = User.new(name: 'a' * 13, email: 'sample@example.com')
+    user.valid?
+    expect(user.errors[:name]).to include("is too long (maximum is 12 characters)")
+  end
+
+  it "異常ケース：emailが256文字以上" do
+    user = User.new(name: 'sample', email: 'a' * 256)
+    user.valid?
+    expect(user.errors[:email]).to include("is too long (maximum is 255 characters)")
+  end
 end
