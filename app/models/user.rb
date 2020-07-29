e049017353c198ba61c1151eaaa4f2998b18a328
K-kind <55728594+K-kind@users.noreply.github.com>
2020-07-29 13:44:25 +0900

    Feat: add user model validation (#21)
    * feat: Add validations to user model
    
    * feat: add unique index to email column

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  validates :name,
+            presence: true,
+            length: { minimum: 2, maximum: 12 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
+  validates :email,
+            presence: true,
+            length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
 end
