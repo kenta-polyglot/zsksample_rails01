05b7f957ac5d3dcbf42d53cf48513f97d621817f
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-08-04 18:16:04 +0900

    test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  it "" do
+    expect(FactoryBot.create(user)).to be_valid
+  end
+
+  let(:user) { Factory.build(:user) }
+
 end
