afdc1296d605695ebf34d564bf7b9282f706c142
keito-sakakibara <62434610+keito-sakakibara@users.noreply.github.com>
2020-08-15 22:23:59 +0900

    feat：userModelのTestを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.new(name: 'Taro', email: 'taro@example.com') }
+
+  
+  it "is valid with a name, user_id, email" do
+    user = User.new(
+      name:    "Sato",
+      email:   "sato@example.com",
+    )
+    expect(user).to be_valid
+  end
+
+  it "is invalid without a name" do
+    user = User.new(name: nil)
+    user.valid?
+    expect(user.errors[:name]).to include("can't be blank")
+  end
+
+  it "is invalid without an email address" do
+    user = User.new(email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
+  end
 end
+
+
+
+
+
+
+
+
