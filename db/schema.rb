b8fa1b2881f26b44326aa736fe14e075d00d94f8
Gozikyu <53566393+Gozikyu@users.noreply.github.com>
2020-10-21 22:03:43 +0900

    test: Userモデルのテストを完成させる (#21)
    * complete_setting_development_environment
    
    * added gitignore file
    
    * added gitingore files
    
    * added gitignore files
    
    * added gitignore files
    
    * complete user test
    
    * edit gitignore file
    
    * edit files to fix errors of rubocop
    
    * edit gitignore file
    
    * restore 3 files
    
    * edit gitignore
    
    * return 3 files to init state

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_08_033844) do
+ActiveRecord::Schema.define(version: 2020_10_15_104010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_07_08_033844) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,70 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'should be vaild' do
+    user = User.new(name: 'hoge', email: 'hoge@gmail.com')
+    expect(user).to be_valid
+  end
+
+  it 'name should be present' do
+    user = User.new(name: '', email: 'hoge@gmail.com')
+    expect(user).not_to be_valid
+  end
+
+  it 'name should be not be too long' do
+    user = User.new(name: 'a' * 51, email: 'hoge@gmail.com')
+    expect(user).not_to be_valid
+  end
+
+  it 'email should be present' do
+    user = User.new(name: 'hoge', email: '')
+    expect(user).not_to be_valid
+  end
+
+  it 'email should be not be too long' do
+    user = User.new(name: 'hoge', email: 'a' * 244 + '@example.com')
+    expect(user).not_to be_valid
+  end
+
+  it 'email validation should accept valid addresses' do
+    valid_addresses = %w[user@example.com
+                         USER@foo.COM
+                         A_US-ER@foo.bar.org
+                         first.last@foo.jp
+                         alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      user = User.new(name: 'hoge', email: valid_address)
+      expect(user).to be_valid
+    end
+  end
+
+  it 'email validation should reject invalid addresses' do
+    invalid_addresses = %w[user@example,com
+                           user_at_foo.org
+                           user.name@example.
+                           foo@bar_baz.com
+                           foo@bar+baz.com
+                           foo@bar..com]
+    invalid_addresses.each do |invalid_address|
+      user = User.new(name: 'hoge', email: invalid_address)
+      expect(user).not_to be_valid
+    end
+  end
+
+  it 'email addresses should be unique' do
+    user = User.new(name: 'hoge', email: 'hoge@gmail.com')
+    duplicate_user = user.dup
+    user.save
+    expect(duplicate_user).not_to be_valid
+  end
+
+  it 'email addresses should be saved as lower-case' do
+    user = User.new(name: 'hoge', email: 'hoge@gmail.com')
+    mixed_case_email = 'HogE@GmaiL.coM'
+    user.email = mixed_case_email
+    user.save
+    expect(user.reload.email).to eq mixed_case_email.downcase
+  end
 end
