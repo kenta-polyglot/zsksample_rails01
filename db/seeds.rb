c49f5849768201a53d7a93a3578c7bbc4cf96e27
sho14kim <shogo14kinjo@gmail.com>
2020-10-25 01:25:42 +0900

    feat: User一覧画面にページネーション機能を追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+50.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n + 1}@railstutorial.org"
+  User.create!(name: name, email: email)
+end
