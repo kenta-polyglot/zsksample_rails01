507640f1d90400328d3d955db23dfb62193900b2
gozikyu <gozikyu@gmail.com>
2020-10-26 08:07:52 +0900

    add validations of micropost

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,2 +1,7 @@
 class Micropost < ApplicationRecord
+  belongs_to :user
+  default_scope -> { order(created_at: :desc) }
+  validates(:user_id, presence: true)
+  validates(:content, presence: true,
+                      length: { maximum: 140 })
 end
