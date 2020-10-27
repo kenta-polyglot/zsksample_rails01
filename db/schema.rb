9586a77f9da9fc4c54fc627604d5aa161f68d2db
Gozikyu <53566393+Gozikyu@users.noreply.github.com>
2020-10-28 07:16:40 +0900

    Test: Micropostモデルのテストを完成させる　+他2つ (#27)
    * complete_setting_development_environment
    
    * add validations of micropost
    
    * edit user model
    
    * add micropost test
    
    * create fixtures files
    
    * add fixtures
    
    * add micropost destroy test to user_spec
    
    * fix rubocop alert
    
    * add index to microposts
    
    * return to initial state
    
    * add test for has_many

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_19_112407) do
+ActiveRecord::Schema.define(version: 2020_10_26_104007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_10_19_112407) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
   end
 
   create_table "users", force: :cascade do |t|
