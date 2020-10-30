0db3195f9b3483cd1a641be2f9852cf0f9bfc24f
sho14kim <shogo14kinjo@gmail.com>
2020-10-30 21:46:00 +0900

    fix: ページネーションのテストを追加

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,5 +7,15 @@ RSpec.describe 'StaticPages', type: :request do
       expect(response).to have_http_status(200)
       expect(response.body).to include 'hello, world!'
     end
+
+    it 'user index including pagination link' do
+      50.times do |n|
+        name  = Faker::Name.name
+        email = "example-#{n + 1}@railstutorial.org"
+        User.create!(name: name, email: email)
+      end
+      get users_url
+      assert_select 'div.pagination'
+    end
   end
 end
