4ea4837bb9d57a0c3512d72459a3ee1779c261e4
kamada-maki <74590047+kamada-maki@users.noreply.github.com>
2020-12-26 16:44:25 +0900

    Test:userリクエストテスト完成 (#16)
    * faker/FactoryBotインストール
    
    * userモデルのmodelバリデーション設定
    
    * requestテストコード実装
    
    * userモデル修正
    
    * new→editアクションに修正

--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,8 @@ ActiveRecord::Schema.define(version: 2020_07_08_033844) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
+    t.string "name", null: false
+    t.string "email", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :user do
+   name                  {Faker::Name.last_name}
+   email                 {Faker::Internet.free_email}
+    trait :invalid do
+      name          {""}
+      email         {""}
+    end
+  end
+end
