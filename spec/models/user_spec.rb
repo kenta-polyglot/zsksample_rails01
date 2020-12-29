5245f495a0ae2b1c37938a3599fc656c1108cfcc
astouKen <75517641+astouKen@users.noreply.github.com>
2020-12-29 23:09:16 +0900

    Merge develop to master (#19)
    * Modify README.md (#27)
    
    * Update README.md
    
    * Update README.md
    
    * Test:userリクエストテスト完成 (#16)
    
    * faker/FactoryBotインストール
    
    * userモデルのmodelバリデーション設定
    
    * requestテストコード実装
    
    * userモデル修正
    
    * new→editアクションに修正
    
    * Add: MicropostsのScaffoldを実行 (#17)
    
    Co-authored-by: mamoru <tiusika.m@gamil.com>
    
    * Test: Userモデルテスト完成 (#18)
    
    * chore/ master.keyの再生成
    
    * add/test/ Userモデル更新+テスト作成
    
    * fix: PR#18のバグ修正
    
    * fix/ 再push用空commit
    
    * fix/ PR#18のバグ修正(作業開始後にdevelopへmergeされた分)
    
    Co-authored-by: Kenta Katsumata <polyglot.software@gmail.com>
    Co-authored-by: kamada-maki <74590047+kamada-maki@users.noreply.github.com>
    Co-authored-by: mamoru02 <58393804+mamoru02@users.noreply.github.com>
    Co-authored-by: mamoru <tiusika.m@gamil.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,61 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.create(:user)
+  end
+
+  describe '有効性の検証' do
+    it 'Userオブジェクトが有効であること' do
+      expect(@user).to be_valid
+    end
+  end
+
+  describe '存在性の検証' do
+    it '名前が存在すること' do
+      @user.name = ' '
+      expect(@user).not_to be_valid
+    end
+    it 'メールアドレスが存在すること' do
+      @user.email = ' '
+      expect(@user).not_to be_valid
+    end
+  end
+
+  describe '入力値の長さの検証' do
+    it '名前が50文字以内であること' do
+      @user.name = 'a' * 51
+      expect(@user).not_to be_valid
+    end
+    it 'メールアドレスが255文字以内であること' do
+      @user.email = 'a' * 244 + '@example.com'
+      expect(@user).not_to be_valid
+    end
+  end
+
+  describe 'emailフォーマットの検証' do
+    it 'メールアドレスのフォーマットが有効であること' do
+      valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+      valid_addresses.each do |valid_address|
+        @user.email = valid_address
+        expect(@user).to be_valid
+      end
+    end
+    it 'メールアドレスのフォーマットが無効であること' do
+      invalid_addresses = %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com]
+      invalid_addresses.each do |invalid_address|
+        @user.email = invalid_address
+        expect(@user).not_to be_valid
+      end
+    end
+  end
+
+  describe 'email一意性の検証' do
+    it '重複したメールアドレスが無効であること' do
+      FactoryBot.create(:user, email: 'zskteam1211@example.com')
+      user = FactoryBot.build(:user, email: 'ZSKTeam1211@example.com')
+      user.valid?
+      expect(user.errors[:email]).to include('has already been taken')
+    end
+  end
 end
