b60e908c699b69c6a088961b132605d0b2bd037a
masakichi-eng <makisankonan@gmail.com>
2021-02-27 10:32:48 +0900

    Micropost 必要ファイル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :microposts
+  validates :name, presence: true    # 「FILL_IN」をコードに置き換えてください
+  validates :email, presence: true    # 「FILL_IN」をコードに置き換えてください
 end
