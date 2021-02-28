cdeabeb608d98b2f81a048d894e62da83ab09b73
masakichi-eng <makisankonan@gmail.com>
2021-02-28 22:56:56 +0900

    refactor:controller

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,5 @@
 class MicropostsController < ApplicationController
   before_action :set_micropost, only: [:show, :edit, :update, :destroy]
-
   # GET /microposts
   # GET /microposts.json
   def index
