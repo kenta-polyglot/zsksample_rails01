6b85f63f52aaccec9c3f8c182acd20367e2401a5
kent7k <kent.ki720@gmail.com>
2021-03-06 19:40:13 +0900

    feat: Micropostの最大文字数を140文字に制限する

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,5 @@
 class Micropost < ApplicationRecord
   belongs_to :user
-  validates :content, length: { maximum: 140 },
-                      presence: true
+  validates :content, length: { maximum: 140 }, # 140文字の文字数制限
+                      presence: true # マイクロポストのコンテンツの存在確認
 end
