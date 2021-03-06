d7165a978f93f684545162f5ce49cc07572a8a6f
kent7k <kent.ki720@gmail.com>
2021-03-06 21:24:51 +0900

    feat: Micropostリクエストのテストを完成させる

---
+++ spec/requests/micropost_request_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe "Microposts", type: :request do
+
+  describe "マイクロポストを作成する" do
+    let(:micropost) { FactoryBot.attributes_for(:micropost) }
+    let(:post_request) { post microposts_path, params: { micropost: micropost } }
+  end
+
+  describe "マイクロポストを削除する" do
+    let!(:micropost) { FactoryBot.create(:micropost) }
+    let(:delete_request) { delete micropost_path(micropost) }
+  end
+end
