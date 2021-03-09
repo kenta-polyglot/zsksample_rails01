17cac4f0d88891949384a955b890573c0781e267
shirokuma-star <takakenhowaken@gmail.com>
2021-03-09 13:48:10 +0900

    Refact: rubocop -A to micropost_request_spec.rb

--- spec/requests/micropost_request_spec.rb
+++ spec/requests/micropost_request_spec.rb
@@ -1,13 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe "Microposts", type: :request do
-
-  describe "マイクロポストを作成する" do
+RSpec.describe 'Microposts', type: :request do
+  describe 'マイクロポストを作成する' do
     let(:micropost) { FactoryBot.attributes_for(:micropost) }
     let(:post_request) { post microposts_path, params: { micropost: micropost } }
   end
 
-  describe "マイクロポストを削除する" do
+  describe 'マイクロポストを削除する' do
     let!(:micropost) { FactoryBot.create(:micropost) }
     let(:delete_request) { delete micropost_path(micropost) }
   end
